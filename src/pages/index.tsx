46aa9dd845b269faa0cbe2459e4471519115ec6d
masumo <mas.kosumo@gmail.com>
2023-07-06 14:46:29 +0700

    fix layout, put alert message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,52 +4,71 @@
 "use client";
 import * as React from 'react';
 import { trpc } from '../utils/trpc';
-import { Button, Typography } from "@material-tailwind/react";
+import { Button, Typography, Spinner, Alert } from "@material-tailwind/react";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
 
 export default function IndexPage() {
-  //const [data, setData] = React.useState(null);
-  const [isWaiting, setWaiting] = React.useState(false);
   
-  const {isLoading, error, data, refetch } = trpc.getOTP.useQuery(
+  const {isFetching, error, data, refetch } = trpc.getOTP.useQuery(
                                                 undefined, 
                                                 {
                                                   refetchOnWindowFocus: false, enabled: false,
                                                 });
-  let otp = null;
-  const handleClick = (setWaiting:any) => {
-    // manually refetch
-    setWaiting(true);
-    refetch();
-    setWaiting(false);
-  };
 
   // 💡 Tip: CMD+Click (or CTRL+Click) on `greeting` to go to the server definition
   //const result = trpc.greeting.useQuery({ name: 'you....' });
-  if (isLoading) return (
-    <div className="flex flex-col justify-center items-center ">
-        <Button className="mt-10 mb-2" onClick={()=>handleClick(setWaiting)}>
-          Display OTP
-        </Button>
-        { 
-          isWaiting? <Typography variant="medium" color="blue-gray" className="text-center mb-2 font-medium">Checking OTP...</Typography> : <p></p>
-        }
-    </div>
-    
-  )
   return (
-          <div className="flex flex-col justify-center items-center ">
+          <div className="h-screen flex flex-col justify-center items-center ">
           {/**
            * The type is defined and can be autocompleted
            * 💡 Tip: Hover over `data` to see the result type
            * 💡 Tip: CMD+Click (or CTRL+Click) on `text` to go to the server definition
            * 💡 Tip: Secondary click on `text` and "Rename Symbol" to rename it both on the client & server
            */}
-          <Button className="mt-10 mb-2" onClick={handleClick}>
+          <Button className="mb-4" onClick={ ()=> refetch()}>
             Display OTP
           </Button>
           { 
-            data? <Typography variant="medium" color="black" className="text-center mb-2 font-medium">{JSON.stringify(data)}</Typography> : <p></p>
+            !isFetching && data? 
+                <Alert
+                color="green"
+                className="max-w-screen-sm"
+                icon={<CheckCircleIcon className="mt-px h-6 w-6" />}
+                >
+                <Typography variant="h5" color="white">
+                  Success
+                </Typography>
+                <Typography color="white" className="mt-2 font-normal">
+                  Your OTP Code: {JSON.stringify(data)}
+                </Typography>
+              </Alert>
+              : <p></p>
           }
+          { 
+          isFetching? 
+                <>
+                    <Typography variant="medium" color="blue-gray" className="text-center mb-4 font-medium">Checking OTP...</Typography>
+                    <Spinner className="h-10 w-10 text-blue-500/10" />
+                </>
+              : <p></p>
+        }
+        { 
+            error? 
+                <Alert
+                color="red"
+                className="max-w-screen-sm"
+                icon={<ExclamationTriangleIcon className="h-6 w-6" />}
+                >
+                <Typography variant="h5" color="white">
+                  Error
+                </Typography>
+                <Typography color="white" className="mt-2 font-normal">
+                  {error.data} <br/> {error.message}
+                </Typography>
+              </Alert>
+              : <p></p>
+        }
         </div>
   );
 }
